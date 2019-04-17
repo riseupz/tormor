2fabdfa82ae311a6f1de87b754b20c04ea05b1fe
Pattarawut <pattarawut@hot-now.com>
2019-04-17 16:55:43 +0700

    Regrouped click command: migrate, sql, enable-modules

--- tormor/commands.py
+++ tormor/commands.py
@@ -1,5 +1,6 @@
 from tormor.exceptions import SchemaNotPresent
 import csv
+import click
 
 # String of queries to add module
 ADD_MODULE = """INSERT INTO module(name) VALUES($1)"""
@@ -21,14 +22,29 @@ CREATE TABLE migration (
     CONSTRAINT migration_pk PRIMARY KEY (module_name, migration)
 );
 """
+@click.group()
+def subcommand():
+    pass
 
-def migrate(conn, dry_run):
+@subcommand.command('migrate')
+@click.pass_context
+@click.option('--dry-run', is_flag=True)
+def migrate(ctx, dry_run):
+    """Run all migrations"""
+
+    conn = ctx.obj['cnx']
     if not dry_run:
         print("NOT DRY RUN")
     else:
         print("DRY RUN")
 
-def enable_modules(conn, modules):
+@subcommand.command('enable-modules')
+@click.pass_context
+@click.argument('modules', required=True, nargs=-1)
+def enable_modules(ctx, modules):
+    """Enable modules"""
+
+    conn = ctx.obj['cnx']
     modules_to_be_added = set(modules)
     try:
         current_modules = conn.load_modules()
@@ -40,16 +56,52 @@ def enable_modules(conn, modules):
         conn.execute(ADD_MODULE.replace("$1", "\'" + each_module +"\'"))
         print(each_module, "enabled")
 
-def execute_sql_file(conn, filename):
+@subcommand.command('sql')
+@click.pass_context
+@click.argument('sqlfile', nargs=1)
+def execute_sql_file(ctx, sqlfile):
+    """
+    Execute SQL queries in files, useful for running migration scripts
+    """
+
     try:
-        with open(filename) as f:
+        conn = ctx.obj['cnx']
+        with open(sqlfile) as f:
             commands = f.read()
             conn.execute(commands)
         conn.load_modules()
-        print(filename, "successfully executed")
+        print(sqlfile, "successfully executed")
     except Exception:
-        print("Error whilst running", filename)
+        print("Error whilst running", sqlfile)
         raise
+
+@subcommand.command()
+@click.pass_context
+@click.argument('filename', required=True, nargs=1)
+def include(ctx, filename):
+    """Run all commands inside a file"""
+    try:
+        with open(filename, newline="") as f:
+            lines = csv.reader(f, delimiter=" ")
+            for each_line in lines:
+                if len(each_line) and not each_line[0].startswith("#"):
+                    cmd = each_line.pop(0)
+                    print(cmd)
+                    if cmd == "migrate":
+                        if len(each_line) == 0:
+                            ctx.invoke(migrate, dry_run = False)
+                        elif len(each_line) == 1:
+                            ctx.invoke(migrate, dry_run = True if each_line[0] == '--dry-run' else False)
+                        else:
+                            raise click.ClickException("Error in migrate command")
+                    elif cmd == "enable-modules":
+                        ctx.invoke(enable_modules, modules = each_line)
+                    elif cmd == "sql" and len(each_line) == 1:
+                        ctx.invoke(execute_sql_file, sqlfile = each_line[0])
+                    else:
+                        raise click.ClickException("Unknown command or parameter")
+    except:
+        print("Error whilst running")
 
 def migrate_sql_file(conn, module, migration, filename):
     return
--- tormor/main_script.py
+++ tormor/main_script.py
@@ -1,6 +1,6 @@
 import click
 from tormor.dsn import makeDSN
-from tormor.commands import enable_modules, migrate, execute_sql_file
+from tormor.commands import enable_modules, migrate, execute_sql_file, include
 from tormor.connections import Connection
 import csv
 
@@ -37,50 +37,14 @@ Command is one of:
 
 """
 
-def include(conn, files):
-    try:
-        for each_file in files:
-            with open(each_file, newline="") as f:
-                lines = csv.reader(f, delimiter=" ")
-                for each_line in lines:
-                    if len(each_line) and not each_line[0].startswith("#"):
-                        cmd = each_line.pop(0)
-                        if cmd == "migrate" and each_line[0] == '--dry-run':
-                            run_command(conn, cmd, None, True)
-                        else:
-                            run_command(conn, cmd, [p for p in each_line if p])
-    except:
-        print("Error whilst running")
-
-# Dictionary of possible commands
-COMMANDS = {
-    "enable-modules": enable_modules,
-    "migrate": migrate,
-    "include": include,
-    "sql": execute_sql_file,
-}
-
-def run_command(cnx, cmd, args, dry_run=False):
-    if cmd == "migrate" and not args:
-        COMMANDS[cmd](cnx, dry_run)
-    elif cmd == "migrate" and args:
-        raise click.ClickException("Error in migrate function")
-    elif not args:
-        raise click.ClickException("Missing parameter for the command")
-    else:
-        COMMANDS[cmd](cnx, args)
-
-@click.command()
-@click.option('-h', nargs=1, required=False)
-@click.option('-d', nargs=1, required=False)
-@click.option('-u', '-U', nargs=1, required=False)
-@click.option('-p', nargs=1, required=False)
-@click.option('-password', '-P', nargs=1, required=False)
-@click.option('--help', is_flag=True)
-@click.argument('command', required=False)
-@click.argument('arguments', required=False, nargs=-1)
-@click.option('--dry-run', is_flag=True)
-def script(h, d, u, p, password, help, command, arguments, dry_run):
+@click.group()
+@click.option('-h', nargs=1, required=False, help="Postgres host")
+@click.option('-d', nargs=1, required=False, help="Database name")
+@click.option('-u', '-U', nargs=1, required=False, help="Database username (role)")
+@click.option('-p', nargs=1, required=False, help="Database port")
+@click.option('-password', '-P', nargs=1, required=False, help="Database password (Not recommended, use environment variable PGPASSWORD instead)")
+@click.pass_context
+def script(ctx, h, d, u, p, password):
     cnx_destination = {
         '-h': h,
         '-d': d,
@@ -89,28 +53,11 @@ def script(h, d, u, p, password, help, command, arguments, dry_run):
         '-P': password
     }
     dsn = makeDSN(cnx_destination)
-    cnx = Connection(dsn)
-    if help:
-        return click.echo(HELPTEXT)
-    # elif command == "migrate" and not dry_run:
-    #     return click.echo(command)
-    # elif command == "migrate" and dry_run:
-    #     return click.echo("dry run migrate")
-    # elif command == "include" and arguments:
-    #     return click.echo(command + str(arguments))
-    # elif command == "include" and not arguments:
-    #     raise click.ClickException("Missing file to be included")
-    # elif command == "enable-modules" and arguments:
-    #     return click.echo(command + str(arguments))
-    # elif command == "enable-modules" and not arguments:
-    #     raise click.ClickException("Missing module name to be enabled")
-    # elif command == "sql" and arguments:
-    #     return click.echo(command + str(arguments))
-    # elif command == "sql" and not arguments:
-    #     raise click.ClickException("Missing sql file to be executed")
-    elif command in COMMANDS:
-        run_command(cnx, command, arguments, dry_run)
-    else:
-        raise click.ClickException("Unknown command")
+    ctx.obj = {'cnx': Connection(dsn)}
+
+script.add_command(migrate)
+script.add_command(enable_modules)
+script.add_command(execute_sql_file)
+script.add_command(include)
 
 # script()
